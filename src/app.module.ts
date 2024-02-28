243cb4d4d3b6e13b8b7420f1e0404b3b470123c1
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-02-28 11:54:38 +0300

    added config module, auth module and uploader module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UploaderModule } from './uploader/uploader.module';
 
 @Module({
-    imports: [AuthModule, UploaderModule],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        AuthModule,
+        UploaderModule,
+    ],
 })
 export class AppModule {}
