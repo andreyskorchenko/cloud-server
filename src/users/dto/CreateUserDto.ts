6d3369fc8e7deae8f042d5ecacb11483d18d9d7c
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-03-10 10:14:24 +0300

    create user dto

---
+++ src/users/dto/CreateUserDto.ts
@@ -0,0 +1,32 @@
+import { IsString, IsNotEmpty, IsEmail, IsStrongPassword } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty({ message: 'First name cannot be empty' })
+    firstname: string;
+
+    @IsString()
+    @IsNotEmpty({ message: 'Last name cannot be empty' })
+    lastname: string;
+
+    @IsString()
+    @IsNotEmpty({ message: 'Nickname cannot be empty' })
+    nickname: string;
+
+    @IsEmail({}, { message: 'Invalid email' })
+    email: string;
+
+    @IsStrongPassword(
+        {
+            minLength: 6,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 0,
+        },
+        {
+            message: 'The minimum password length is 6 characters, one number, a lowercase and an uppercase letter',
+        },
+    )
+    password: string;
+}
