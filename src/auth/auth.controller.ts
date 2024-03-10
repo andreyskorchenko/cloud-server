61a08128925589e34a547a6e2f1510f97e660bf1
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-03-10 15:24:22 +0300

    implemented controller for sign in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Post, UsePipes, ValidationPipe, Body } from '@nestjs/common';
+import { Controller, Post, HttpCode, UsePipes, ValidationPipe, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/CreateUserDto';
+import { SigninUserDto } from './dto/SigninUserDto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
+    @Post('signin')
+    @HttpCode(200)
+    @UsePipes(new ValidationPipe())
+    async signIn(@Body() userDto: SigninUserDto) {
+        return await this.authService.signIn(userDto);
+    }
+
     @Post('signup')
     @UsePipes(new ValidationPipe())
-    async signup(@Body() userDto: CreateUserDto) {
-        return await this.authService.create(userDto);
+    async signUp(@Body() userDto: CreateUserDto) {
+        return await this.authService.signUp(userDto);
     }
 }
