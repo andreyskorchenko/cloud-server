88da0c94fc79cc201a760b7e2072c3252beb5b43
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-03-11 12:51:52 +0300

    added JwtOptions config

---
+++ src/auth/config/JwtOptions.ts
@@ -0,0 +1,14 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModuleAsyncOptions } from '@nestjs/jwt';
+
+export const JwtOptions: JwtModuleAsyncOptions = {
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+            expiresIn: '15m',
+        },
+    }),
+};
