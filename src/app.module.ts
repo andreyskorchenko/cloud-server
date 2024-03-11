c60d1d366458f7b5f8fd9d0708ab4743df0dfa3e
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-03-11 12:52:32 +0300

    use MongooseOptions config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from '@/users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseOptions } from './config/MongooseOptions';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
         }),
-        MongooseModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService) => ({
-                uri: 'mongodb://localhost:27027',
-                dbName: configService.get('DB_NAME'),
-                auth: {
-                    username: configService.get('DB_USERNAME'),
-                    password: configService.get('DB_PASSWORD'),
-                },
-            }),
-        }),
+        MongooseModule.forRootAsync(MongooseOptions),
         AuthModule,
         UsersModule,
     ],
