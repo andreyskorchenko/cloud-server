ae970a439d3a8da04c4c13c0071c0020ca57d87c
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-03-20 10:46:43 +0300

    improve auth module added passport and jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UsersModule } from '@/users/users.module';
-import { JwtOptions } from './config/JwtOptions';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '@/users';
+import { AuthController, AuthService } from '@/auth';
+import { JwtStrategy } from '@/auth/strategies';
+import { JwtOptions } from '@/auth/config';
 
 @Module({
-    imports: [JwtModule.registerAsync(JwtOptions), UsersModule],
+    imports: [JwtModule.registerAsync(JwtOptions), UsersModule, PassportModule],
     controllers: [AuthController],
-    providers: [AuthService],
+    providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
