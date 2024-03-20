bb7329fe446fa9abbdff9e0a805d74c5e1eb1aa4
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-03-20 10:47:34 +0300

    improve auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, HttpCode, UsePipes, ValidationPipe, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from '../users/dto/CreateUserDto';
+import { CreateUserDto } from '@/users/dto/CreateUserDto';
 import { SigninUserDto } from './dto/SigninUserDto';
 
 @Controller('auth')
@@ -10,13 +10,13 @@ export class AuthController {
     @Post('signin')
     @HttpCode(200)
     @UsePipes(new ValidationPipe())
-    async signIn(@Body() userDto: SigninUserDto) {
-        return await this.authService.signIn(userDto);
+    async signin(@Body() userDto: SigninUserDto) {
+        return await this.authService.signin(userDto);
     }
 
     @Post('signup')
     @UsePipes(new ValidationPipe())
-    async signUp(@Body() userDto: CreateUserDto) {
-        return await this.authService.signUp(userDto);
+    async signup(@Body() userDto: CreateUserDto) {
+        return await this.authService.signup(userDto);
     }
 }
