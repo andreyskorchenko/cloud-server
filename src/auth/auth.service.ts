c6b0599f9f86618a9dcc62d7e2c6517076ad247f
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-03-31 17:22:49 +0300

    implemented methods signin and signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,51 @@
-import { genSalt, hash } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { genSalt, hash, compare } from 'bcrypt';
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
-import { CreateUserDto } from '../users/dto/CreateUserDto';
+import { UsersService } from '@/users/users.service';
+import { CreateUserDto } from '@/users/dto/CreateUserDto';
+import { SigninUserDto } from '@/auth/dto/SigninUserDto';
+import { JwtPayload } from '@/auth/interfaces';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+    constructor(
+        private jwtService: JwtService,
+        private usersService: UsersService,
+        private configService: ConfigService,
+    ) {}
 
-    async create(userDto: CreateUserDto) {
-        const isUserExists = await this.usersService.findOne({
+    async signin({ login, password }: SigninUserDto) {
+        const candidate = await this.usersService.findOneByNicknameOrEmail({
+            nickname: login,
+            email: login,
+        });
+
+        if (!candidate) {
+            throw new HttpException('UNAUTHORIZED', HttpStatus.UNAUTHORIZED);
+        }
+
+        const isComparePasswords = await compare(password, candidate.password);
+        if (!isComparePasswords) {
+            throw new HttpException('UNAUTHORIZED', HttpStatus.UNAUTHORIZED);
+        }
+
+        const jwtPayload: JwtPayload = {
+            uid: candidate._id.toString(),
+            nickname: candidate.nickname,
+            roles: candidate.roles,
+        };
+
+        const accessToken = await this.jwtService.signAsync(jwtPayload);
+        const refreshToken = await this.jwtService.signAsync(jwtPayload, {
+            expiresIn: this.configService.get('JWT_EXPIRESIN_REFRESH'),
+        });
+
+        return { accessToken, refreshToken };
+    }
+
+    async signup(userDto: CreateUserDto) {
+        const isUserExists = await this.usersService.findOneByNicknameOrEmail({
             nickname: userDto.nickname,
             email: userDto.email,
         });
