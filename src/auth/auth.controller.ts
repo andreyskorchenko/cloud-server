1c665bfd20990c7fc62185427b44cb53547c663e
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-04-04 10:42:47 +0300

    improve and use browser fingerprint decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Post, HttpCode, UsePipes, ValidationPipe, Body } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { CreateUserDto } from '@/users/dto/CreateUserDto';
-import { SigninUserDto } from './dto/SigninUserDto';
+import { BrowserFingerprint } from '@/auth/decorators';
+import { CreateUserDto } from '@/users/dto';
+import { SigninUserDto } from '@/auth/dto';
+import { AuthService } from '@/auth';
 
 @Controller('auth')
 export class AuthController {
@@ -10,13 +11,13 @@ export class AuthController {
     @Post('signin')
     @HttpCode(200)
     @UsePipes(new ValidationPipe())
-    async signin(@Body() userDto: SigninUserDto) {
-        return await this.authService.signin(userDto);
+    async signin(@Body() userDto: SigninUserDto, @BrowserFingerprint() fingerprint: null | string) {
+        return await this.authService.signin(userDto, fingerprint);
     }
 
     @Post('signup')
     @UsePipes(new ValidationPipe())
-    async signup(@Body() userDto: CreateUserDto) {
-        return await this.authService.signup(userDto);
+    async signup(@Body() userDto: CreateUserDto, @BrowserFingerprint() fingerprint: null | string) {
+        return await this.authService.signup(userDto, fingerprint);
     }
 }
