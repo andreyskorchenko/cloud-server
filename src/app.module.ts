2da1ef05540c7b920509115bd91a0a21ce67c313
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-04-04 11:02:40 +0300

    improve imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from '@/users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { MongooseOptions } from './config/MongooseOptions';
+import { MongooseOptions } from '@/config';
+import { UsersModule } from '@/users';
+import { AuthModule } from '@/auth';
 
 @Module({
     imports: [
