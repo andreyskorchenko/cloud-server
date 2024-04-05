848185975b3a8b562d7137e482b225e6374f206b
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-04-05 11:17:38 +0300

    route for email confirmation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UsePipes, ValidationPipe, Param } from '@nestjs/common';
+import { EmailConfirmationDto } from '@/users/dto';
 import { UsersService } from '@/users/users.service';
 
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) {}
+
+    @Get('email-confirmation/:token')
+    @UsePipes(new ValidationPipe())
+    async emailConfirmation(@Param() { token }: EmailConfirmationDto) {
+        return await this.usersService.emailConfirmation(token);
+    }
 }
