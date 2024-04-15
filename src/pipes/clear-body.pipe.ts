024cc397dae2fe7b4576165b7eac1af7857f0013
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-04-15 11:09:07 +0300

    refactoring pipe for clear request body

--- src/pipes/clear-body.pipe.ts
+++ src/pipes/clear-body.pipe.ts
@@ -1,16 +1,11 @@
 import { Injectable, PipeTransform } from '@nestjs/common';
 import * as striptags from 'striptags';
 
-type Body = Record<string, string>;
-
 @Injectable()
 export class ClearBodyPipe implements PipeTransform {
-    transform(value: Body) {
-        const body: Body = {};
-        for (const key in value) {
-            body[key] = striptags(value[key]).trim();
-        }
-
-        return body;
+    transform(body: Record<string, string>) {
+        return Object.entries(body).reduce((acc, [key, value]) => {
+            return { ...acc, [key]: striptags(value).trim() };
+        }, {});
     }
 }
