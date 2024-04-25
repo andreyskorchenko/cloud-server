47548c36e1c07f53f1c20da5f1bad84cfc1d0411
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-04-25 16:05:29 +0300

    rename schema to schemas

---
+++ src/storage/schemas/storage.schema.ts
@@ -0,0 +1,15 @@
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { DirectoryStorage } from '@/storage/types';
+
+@Schema()
+class Storage {
+    @Prop({ required: true })
+    storage: DirectoryStorage[];
+
+    @Prop({ required: true, unique: true, ref: 'users' })
+    userId: mongoose.Schema.Types.ObjectId;
+}
+
+export type StorageDocument = HydratedDocument<Storage>;
+export const StorageSchema = SchemaFactory.createForClass(Storage);
