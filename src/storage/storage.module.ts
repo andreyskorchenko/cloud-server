a4ec0fe7d3f8e4c62227f3745b45d72a05aa6f31
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-04-25 16:08:17 +0300

    implemented storage module

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StorageSchema } from '@/storage/schemas';
+import { StorageController } from '@/storage/storage.controller';
+import { StorageService } from '@/storage/storage.service';
 
-@Module({})
+@Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: 'storages',
+                schema: StorageSchema,
+            },
+        ]),
+    ],
+    controllers: [StorageController],
+    providers: [StorageService],
+    exports: [StorageService],
+})
 export class StorageModule {}
