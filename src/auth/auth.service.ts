8727eded0bf1a53829c69e4598e2444b1658ae49
Andrey Skorchenko <andreyskorchenko@gmail.com>
2024-05-03 17:09:54 +0300

    preparing the signin method for use OTP

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,8 +3,8 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { UsersService } from '@/users/users.service';
 import { CreateUserDto } from '@/users/dto';
 import { SigninUserDto } from '@/auth/dto';
-import { JwtPayload } from '@/auth/interfaces';
 import { TokenService } from '@/token/token.service';
+import { generateOtp } from '@/auth/helpers';
 
 @Injectable()
 export class AuthService {
@@ -13,7 +13,7 @@ export class AuthService {
         private readonly tokenService: TokenService,
     ) {}
 
-    async signin({ login, password }: SigninUserDto, fingerprint: string | null) {
+    async signin({ login, password }: SigninUserDto) {
         const user = await this.usersService.find({ nickname: login, email: login }).one();
 
         if (!user) {
@@ -25,27 +25,8 @@ export class AuthService {
             throw new HttpException('UNAUTHORIZED', HttpStatus.UNAUTHORIZED);
         }
 
-        const jwtPayload: JwtPayload = {
-            id: user._id.toString(),
-            nickname: user.nickname,
-            roles: user.roles,
-        };
-
-        const accessToken = await this.tokenService.generateAccess(jwtPayload);
-        const refreshToken = await this.tokenService.generateRefresh(jwtPayload);
-
-        if (!accessToken || !refreshToken) {
-            throw new HttpException('', HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-        await this.usersService.addDevice(user, refreshToken, fingerprint);
-
-        return {
-            accessToken,
-            refreshToken,
-            nickname: user.nickname,
-            roles: user.roles,
-        };
+        const otp = generateOtp();
+        return await this.usersService.setOtp(user.id, otp);
     }
 
     async signup(userDto: CreateUserDto, fingerprint: string | null) {
